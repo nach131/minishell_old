0efdb67a53f854720ebe46663b1d020ebff92894
nacho <nacho.mota@gmail.com>
2023-05-07 18:01:44 +0200

    cambio define por enum

--- sources/inc/minishell.h
+++ sources/inc/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: caguerre <caguerre@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 15:08:47 by caguerre          #+#    #+#             */
-/*   Updated: 2023/05/02 17:08:54 by caguerre         ###   ########.fr       */
+/*   Updated: 2023/05/07 18:00:56 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "functions.h"
 # include "libft.h"
-# include <errno.h>
+# include <error.h>
 # include <fcntl.h>
 # include <limits.h>
 # include <readline/history.h>
@@ -35,36 +35,34 @@ extern int		gbl_exit_code;
 
 // MACROS
 
-# define SUCCESS 0
-# define FAILURE 1
-
-# define CMD_NOT_FOUND 127
-
-# ifndef PATH_MAX
-#  define PATH_MAX 1024
-# endif
+enum
+{
+	SUCCESS,
+	FAILURE,
+	PATH_MAX = 1024
+}
 
 // STRUCTS
 
 typedef struct s_fds
 {
-    char        *in_file;
-    char        *out_file;
-}   t_fds;
+	char		*in_file;
+	char		*out_file;
+}				t_fds;
 
 typedef struct s_command
 {
-    char        *command;
-    char        **args;
-    char        *path;
-    t_fds       *fds;
-}   t_command;
+	char		*command;
+	char		**args;
+	char		*path;
+	t_fds		*fds;
+}				t_command;
 
 typedef struct s_data
 {
 	bool		interactive;
 	t_command	*cmd;
 	pid_t		pid;
-}	t_data;
+}				t_data;
 
 #endif
