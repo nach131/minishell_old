10f5baed5f9778fe65d677ef581dae8e8bd29e24
nacho <nacho.mota@gmail.com>
2023-05-09 20:01:34 +0200

    fork

--- funciones/permitidas/fork.c
+++ funciones/permitidas/fork.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   access.c                                           :+:      :+:    :+:   */
+/*   fork.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/09 16:38:34 by nmota-bu          #+#    #+#             */
-/*   Updated: 2023/05/09 19:55:30 by nmota-bu         ###   ########.fr       */
+/*   Created: 2023/05/09 19:55:22 by nmota-bu          #+#    #+#             */
+/*   Updated: 2023/05/09 20:01:16 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,60 @@
 /* ╚════════════════════════════════════════════════════════════════════════╝ */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-
-// int	main(void)
-// {
-// 	// Verifica si el archivo existe
-// 	if (access("archivo.txt", F_OK) != -1)
-// 		printf("El archivo existe.\n");
-// 	else
-// 		printf("El archivo no existe.\n");
-// 	return (0);
-// }
 
 int	main(void)
 {
-	// Verifica si el archivo es escribible y ejecutable
-	if (access("archivo.txt", W_OK | X_OK) == 0)
-		printf("El archivo es escribible y ejecutable.\n");
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		printf("Error al crear proceso hijo\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		printf("Proceso hijo creado\n");
+		printf("ID del proceso hijo: %d\n", getpid());
+		execl("/bin/ls", "ls", "-l", NULL);
+		printf("Error al ejecutar el comando\n");
+		exit(EXIT_FAILURE);
+	}
 	else
-		printf("El archivo no es escribible y ejecutable.\n");
+	{
+		printf("Proceso padre\n");
+		printf("ID del proceso padre: %d\n", getpid());
+	}
 	return (0);
 }
+//=========================================================================
+// int	main(void)
+// {
+// 	pid_t pid;
+// 	int x = 10;
+
+// 	pid = fork();
+// 	if (pid == -1)
+// 	{
+// 		printf("Error al crear proceso hijo\n");
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	else if (pid == 0)
+// 	{
+// 		printf("Proceso hijo creado\n");
+// 		printf("Valor de x en el proceso hijo: %d\n", x);
+// 		x += 5;
+// 		printf("Nuevo valor de x en el proceso hijo: %d\n", x);
+// 	}
+// 	else
+// 	{
+// 		printf("Proceso padre\n");
+// 		printf("Valor de x en el proceso padre: %d\n", x);
+// 		x += 10;
+// 		printf("Nuevo valor de x en el proceso padre: %d\n", x);
+// 	}
+
+// 	return (0);
+// }
