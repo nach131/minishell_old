e8a6d399460448a7fa6034b594a255a4a1bda5fc
nacho <nacho.mota@gmail.com>
2023-05-25 11:52:55 +0200

    ft_isnumber modificada

--- sources/inc/builtins.h
+++ sources/inc/builtins.h
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 15:57:38 by nmota-bu          #+#    #+#             */
-/*   Updated: 2023/05/24 16:14:48 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/05/25 11:38:48 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 
 void	print_env(t_list *env);
 int		execute_builtin(t_data *data, t_cmd *cmd);
+int		ft_isnumber(const char *str);
 
 // WORKS
 
--- sources/src/built-ins/exit_btin.c
+++ sources/src/built-ins/exit_btin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit_btin.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: carles <carles@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 20:08:46 by carles            #+#    #+#             */
-/*   Updated: 2023/05/24 21:40:39 by carles           ###   ########.fr       */
+/*   Updated: 2023/05/25 11:48:10 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,50 +15,44 @@
 
 // Esto simplemente para verificar que el argumento es un número válido
 //
-int ft_isnumber(const char *str)
+int	ft_isnumber(const char *str)
 {
-    int i;
-
-    i = 0;
-    if (!str)
-        return (NULL);
-    if (str[0] == '-')
-        i++;
-    while (str[i])
-    {
-        if (str[i] < '0' || str[i] > '9')
-            return (0);
-        i++;
-    }
-    return (1);
+	if (*str == '-')
+		str++;
+	else if (*str == '+')
+		str++;
+	if (ft_isdigit(*str))
+		return (TRUE);
+	return (FALSE);
 }
 
 // Lo primero que hacemos es indicar que hay que salir del programa. Luego
 // imprimimos exit en la salida de error y utilizamos un operador ternario
-// para verificar si args[1] está presente o no... Si lo está, buena cara y 
+// para verificar si args[1] está presente o no... Si lo está, buena cara y
 // si no lo está, cara de asco. Luego hacemos unas comprobaciones por si
 // se han dado demasiados argumentos o si args[1] es o no un número válido.
 // Si es válido usamos atoi e imprimimos lo que toca.
 //
-int exit_btin(t_data *data, char **args)
+int	exit_btin(t_data *data, char **args)
 {
-    data->exit = 1;
-    ft_putstr_fd("exit ", 2);
-    args[1] ? ft_putendl_fd("😀", 2) : ft_putendl_fd("🤢", 2);
-    if (args[1] && args[2])
-    {
-        data->ret = 1;
-        ft_putendl_fd("exit: too many argument", 2);
-    }
-    else if (args[1] && ft_isnumber(args[1]) == 0)
-    {
-        data->ret = 255;
-        ft_putstr_fd("exit: ", 2);
-        ft_putstr_fd(args[1], 2);
-        ft_putendl_fd(": argument is not numeric", 2);
-    }
-    else if (args[1])
-        data->ret = ft_atoi(args[1]);
-    else
-        data->ret = 0;
+	data->exit = 1;
+	ft_putstr_fd("exit ", 2);
+	args[1] ? ft_putendl_fd("😀", 2) : ft_putendl_fd("🤢", 2);
+	if (args[1] && args[2])
+	{
+		data->ret = 1;
+		ft_putendl_fd("exit: too many argument", 2);
+	}
+	else if (args[1] && ft_isnumber(args[1]))
+	{
+		data->ret = 255;
+		ft_putstr_fd("exit: ", 2);
+		ft_putstr_fd(args[1], 2);
+		ft_putendl_fd(": argument is not numeric", 2);
+	}
+	else if (args[1])
+		data->ret = ft_atoi(args[1]);
+	else
+		data->ret = 0;
+	return (1); // TIENE QUE DEVOLVER INT
 }
