51f6d9be0c01d3721567c4fd050386c1ecbdf967
nacho <nacho.mota@gmail.com>
2023-07-20 18:43:31 +0200

    limpieza init_cmd

--- sources/src/cmd/init_cmd.c
+++ sources/src/cmd/init_cmd.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 12:35:20 by nmota-bu          #+#    #+#             */
-/*   Updated: 2023/07/20 18:13:33 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/07/20 18:40:30 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,36 +17,6 @@
 
 #include "cmd.h"
 #include "minishell.h"
-
-// void static	commands(t_cmd *cmd, t_list *token)
-// {
-// 	int	i;
-// 	int	valid_cmds;
-
-// 	i = 0;
-// 	valid_cmds = 0;
-// 	// Contador para rastrear el número de comandos válidos agregados
-// 	cmd->command = malloc((cmd->num_cmd + 1) * sizeof(char *));
-// 	while (token != NULL)
-// 	{
-// 		// Ignorar "|", "<" y ">"
-// 			if (token->content[0] != '|' || token->content[0] != '>' || token->content[0] != '<')
-// 			{
-// 				token = token->next;
-// 				continue;
-// 			}
-// 		if (i % 2 == 0)
-// 		{
-// 			// Agregar el primer valor de la lista
-// 			// cmd->command[valid_cmds] = access_file(token->content);
-// 			cmd->command[valid_cmds] = ft_strdup(token->content);
-// 			valid_cmds++;
-// 		}
-// 		token = token->next;
-// 		i++;
-// 	}
-// 	cmd->command[valid_cmds] = NULL;
-// }
 
 void commands(t_cmd *cmd, t_list *token)
 {
@@ -61,7 +31,8 @@ void commands(t_cmd *cmd, t_list *token)
 	{
 		if (current->content[0] != '-' && current->content[0] != '<' && current->content[0] != '>' && current->content[0] != '|' && flag)
 		{
-			cmd->command[i] = strdup(current->content);
+			// cmd->command[i] = strdup(current->content);
+			cmd->command[i] = access_file(current->content);
 			i++;
 			flag = 0;
 		}
@@ -71,60 +42,61 @@ void commands(t_cmd *cmd, t_list *token)
 	}
 
 	cmd->command[i] = NULL;
+}
 
-	// void static	args(t_cmd *cmd, t_list *token)
-	// {
-	// 	int		num_args;
-	// 	int		arg_index;
-	// 	t_list	*current_token;
-	// 	int		arg_length;
-	// 	t_list	*arg_token;
-	// 	int		i;
+// void static	args(t_cmd *cmd, t_list *token)
+// {
+// 	int		num_args;
+// 	int		arg_index;
+// 	t_list	*current_token;
+// 	int		arg_length;
+// 	t_list	*arg_token;
+// 	int		i;
 
-	// 	num_args = cmd->num_cmd + 1;
-	// 	cmd->args = malloc((num_args) * sizeof(char *));
-	// 	arg_index = 0;
-	// 	current_token = token;
-	// 	while (current_token != NULL)
-	// 	{
-	// 		if (!ft_strncmp(current_token->content, "|", 1)
-	// 			|| !ft_strncmp(current_token->content, ">", 1)
-	// 			|| !ft_strncmp(current_token->content, "<", 1))
-	// 		{
-	// 			current_token = current_token->next;
-	// 			continue ;
-	// 		}
-	// 		arg_length = 0;
-	// 		arg_token = current_token;
-	// 		while (arg_token != NULL && ft_strncmp(arg_token->content, "|", 1)
-	// 			&& ft_strncmp(arg_token->content, ">", 1)
-	// 			&& ft_strncmp(arg_token->content, "<", 1))
-	// 		{
-	// 			arg_length++;
-	// 			arg_token = arg_token->next;
-	// 		}
-	// 		cmd->args[arg_index] = malloc((arg_length + 1) * sizeof(char *));
-	// 		cmd->args[arg_index][arg_length] = NULL;
-	// 		i = 0;
-	// 		while (i < arg_length)
-	// 		{
-	// 			cmd->args[arg_index][i] = ft_strdup(current_token->content);
-	// 			current_token = current_token->next;
-	// 			i++;
-	// 		}
-	// 		arg_index++;
-	// 	}
-	// 	cmd->args[arg_index] = NULL;
-	// }
+// 	num_args = cmd->num_cmd + 1;
+// 	cmd->args = malloc((num_args) * sizeof(char *));
+// 	arg_index = 0;
+// 	current_token = token;
+// 	while (current_token != NULL)
+// 	{
+// 		if (!ft_strncmp(current_token->content, "|", 1)
+// 			|| !ft_strncmp(current_token->content, ">", 1)
+// 			|| !ft_strncmp(current_token->content, "<", 1))
+// 		{
+// 			current_token = current_token->next;
+// 			continue ;
+// 		}
+// 		arg_length = 0;
+// 		arg_token = current_token;
+// 		while (arg_token != NULL && ft_strncmp(arg_token->content, "|", 1)
+// 			&& ft_strncmp(arg_token->content, ">", 1)
+// 			&& ft_strncmp(arg_token->content, "<", 1))
+// 		{
+// 			arg_length++;
+// 			arg_token = arg_token->next;
+// 		}
+// 		cmd->args[arg_index] = malloc((arg_length + 1) * sizeof(char *));
+// 		cmd->args[arg_index][arg_length] = NULL;
+// 		i = 0;
+// 		while (i < arg_length)
+// 		{
+// 			cmd->args[arg_index][i] = ft_strdup(current_token->content);
+// 			current_token = current_token->next;
+// 			i++;
+// 		}
+// 		arg_index++;
+// 	}
+// 	cmd->args[arg_index] = NULL;
+// }
 
-	void init_cmd(t_list * token, t_list * env, t_cmd * cmd)
-	{
-		cmd->num_cmd = count_commands(token) + 1;
-		commands(cmd, token);
-		// args(cmd, token);
-		cmd->env = ft_lst_to_dptr(&env, 0);
-		process_redirections(cmd, token);
-		pipe_to_cmd(cmd);
+void init_cmd(t_list *token, t_list *env, t_cmd *cmd)
+{
+	cmd->num_cmd = count_commands(token) + 1;
+	commands(cmd, token);
+	// args(cmd, token);
+	cmd->env = ft_lst_to_dptr(&env, 0);
+	process_redirections(cmd, token);
+	pipe_to_cmd(cmd);
 }
 
 // ANTIC
