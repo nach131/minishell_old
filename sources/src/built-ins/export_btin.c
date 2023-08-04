2476da221240fbe2ed3623bfec799f230ea6a836
nacho <nacho.mota@gmail.com>
2023-08-04 12:14:16 +0200

    limpieza

--- sources/src/built-ins/export_btin.c
+++ sources/src/built-ins/export_btin.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 11:50:41 by nmota-bu          #+#    #+#             */
-/*   Updated: 2023/08/04 12:11:35 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/08/04 12:13:58 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,51 +17,6 @@
 
 #include "libft.h"
 #include "minishell.h"
-
-// int compare_env_strings(const void *a, const void *b)
-// {
-// 	// The environment strings are in the format "VARIABLE=value"
-// 	// We need to compare only the VARIABLE part, so find the '=' character.
-// 	const char *str_a = *(const char **)a;
-// 	const char *str_b = *(const char **)b;
-// 	// const char *equals_a = strchr(str_a, '=');
-// 	// const char *equals_b = strchr(str_b, '=');
-
-// 	// return ft_strcmp((char *)str_a, (char *)str_b);
-// 	return strcmp(str_a, str_b);
-// }
-
-// void custom_qsort(void *base, size_t num_elements, size_t element_size, int (*compare)(const void *, const void *))
-// {
-// 	char *arr = (char *)base;
-// 	size_t i = 0;
-// 	char *temp = (char *)malloc(element_size);
-
-// 	while (i < num_elements - 1)
-// 	{
-// 		size_t j = i + 1;
-// 		while (j < num_elements)
-// 		{
-// 			char *element_a = arr + i * element_size;
-// 			char *element_b = arr + j * element_size;
-
-// 			if (compare(element_a, element_b) > 0)
-// 			// if (strcmp(element_a, element_b) > 0)
-// 			{
-// 				memcpy(temp, element_a, element_size);
-// 				memcpy(element_a, element_b, element_size);
-// 				memcpy(element_b, temp, element_size);
-// 			}
-
-// 			j++;
-// 		}
-
-// 		i++;
-// 	}
-
-// 	free(temp);
-// }
-//=========================================================================
 
 void print_dptr(char **arr)
 {
